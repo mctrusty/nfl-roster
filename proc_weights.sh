21ee1170394545a6d14188dc3bd7fbebc1ef294f
mike <mike@mike-lappy.(none)>
2013-09-19 19:36:28 -0600

    removed -P32 line from xargs for the grep line bc the parallel execution of that line was causing issues with the final output

--- proc_weights.sh
+++ proc_weights.sh
@@ -14,9 +14,9 @@ mkdir $dirname
 #http://stackoverflow.com/questions/7577615/parallel-wget-in-bash
 cat nfl_team_roster_pages.txt | sed 's_http://www\.\([a-z0-9]*\)\.com.*_-P'$dirname'/\1 &_' | xargs -n 2 -P 16 wget 
 
-find $dirname/ -type f -print0 | xargs -n1 -0 -P32 grep -H col-weight\"\>[0-9] > weightlines.txt
+find $dirname/ -type f -print0 | xargs -n1 -0 grep -H col-weight\"\>[0-9] > weightlines.txt
 
-sed -r 's_.*/([a-z]*)/.*([0-9][0-9][0-9]).*_\1 \2_' < weightlines.txt > weights.txt
+sed -r 's_.*/([a-z0-9]*)/.*([0-9][0-9][0-9]).*_\1 \2_' < weightlines.txt > weights.txt
 
 
 
