303c57c2b205cdf5ee7128a78659032ddea44b24
Michael J Cox <mike.jon.cox@gmail.com>
2013-09-23 21:12:25 -0600

    Update results.js
    cleanup

--- results.js
+++ results.js
@@ -5,22 +5,13 @@ var page = require('webpage').create();
 page.open(url, function(status) {
  
     if (status === 'success') {
- 
-        // Run a function in the webpage's context and catch what it returns.
         var html = page.evaluate(function() {
-            // Optionally, do some more page manipulation here.
-            // ...
- 
-            // Return the HTML from the loaded and JS-manipulated page.
-            // Note that a console.log() here in this context won't be visible (by default).
-			table2csv('games');
+	    table2csv('games');
             return document.getElementById('div_games').textContent;//documentElement.outerHTML;
         });
  
-        // Print the HTML to standard output.
         console.log(html);
     }
- 
-    // Make sure we quit PhantomJS, no point to keep waiting for nothing.
+
     phantom.exit();
 });
